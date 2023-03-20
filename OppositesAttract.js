b793a3ff575a48962fb2e0f8b35097efe8843d0d
leronaden <lenka200909@gmail.com>
2023-03-20 10:55:18 +0100

    Update OppositesAttract.js

--- OppositesAttract.js
+++ OppositesAttract.js
@@ -1,15 +1,7 @@
-/* OppositesAttract */
-
-public class OppositesAttract {
-
-    public static boolean isLove(final int flower1, final int flower2) {
-      boolean inLove = false;
-      boolean petals1, petals2;
-      petals1 = (flower1 % 2) == 0;
-      petals2 = (flower2 % 2) == 0;
-      if (petals1 != petals2) {
-        inLove = true;
-      }
-      return inLove;
-    }  
-  } 
+function lovefunc(flower1,flower2){
+    if (flower1%2==0&&flower2%2!==0) {
+        return true;
+    }else if(flower1%2!==0&&flower2%2==0){
+        return true;
+    }else return false;
+}
