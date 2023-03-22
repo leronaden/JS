35439e1f584375f0748166d354bdc8199aaf217f
leronaden <lenka200909@gmail.com>
2023-03-22 12:23:58 +0100

    Create convert a number to a string-1.js

---
+++ convert a number to a string-1.js
@@ -0,0 +1,10 @@
+/* We need a function that can transform a number (integer) into a string.
+
+To convert an integer to a string, use the str() built-in function. The function takes an integer (or other type) as its input and produces a string as its output.*/
+
+
+
+function numberToString(num) {
+    return String(num)
+    // Return a string of the number here!
+  }
