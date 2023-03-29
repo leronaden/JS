906760f5bee89d3f6ffa5d12471a217b64f30e2c
leronaden <lenka200909@gmail.com>
2023-03-29 11:37:13 +0200

    Update Create complex multi-dimensional arrays.js

--- Create complex multi-dimensional arrays.js
+++ Create complex multi-dimensional arrays.js
@@ -1,11 +1,13 @@
-let myNestedArray = [
-    [
-      ['deep'],'unshift', false, 1, 2, 3, 'complex', 'nested',
-      [
-        ['deeper'],
-        [
-           ['deepest'],
-        ]
-      ],
-    ],
-  ];
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+  };
+  
+  // Only change code below this line
+  foods.bananas = 13;
+  foods.grapes = 35;
+  foods.strawberries = 27;
+  // Only change code above this line
+  
+  console.log(foods);
