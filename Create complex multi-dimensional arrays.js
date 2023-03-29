818deeb3072620bf697b0c35595520c21fbf3be0
leronaden <lenka200909@gmail.com>
2023-03-29 11:36:39 +0200

    Create Create complex multi-dimensional arrays.js

---
+++ Create complex multi-dimensional arrays.js
@@ -0,0 +1,11 @@
+let myNestedArray = [
+    [
+      ['deep'],'unshift', false, 1, 2, 3, 'complex', 'nested',
+      [
+        ['deeper'],
+        [
+           ['deepest'],
+        ]
+      ],
+    ],
+  ];
